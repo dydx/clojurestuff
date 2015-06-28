49826e77235815d209bae0163b3edb068d17d7c4
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-06-28 00:07:04 -0400

    accessing stuff in vectors. odd note- if failed-protagonist-names is defined after -main, i get nil back

--- src/demo/core.clj
+++ src/demo/core.clj
@@ -1,13 +1,10 @@
 (ns demo.core
   (:gen-class))
 
-(defn -main [& args]
-  (println (when true?
-    (println "Success!")
-    "abra cadabra")))
-
-
 (def failed-protagonist-names
   ["Larry Potter"
    "Doreen the Explorer"
    "The Incredible Bulk"])
+
+(defn -main [& args]
+  (println (get failed-protagonist-names 1)))
