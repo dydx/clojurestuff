767a83ce832752c7920397072addc480f6796978
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-06-28 00:12:44 -0400

    fixed repo path

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject demo "0.1.0-SNAPSHOT"
   :description "demo app with leinengin"
-  :url "https://github.com/dydx/demo"
+  :url "https://github.com/dydx/clojurestuff"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
