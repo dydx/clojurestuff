4bcc018158590747e47f6234bd059c9aa775ea70
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-06-27 23:02:15 -0400

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject demo "0.1.0-SNAPSHOT"
+  :description "demo app with leinengin"
+  :url "https://github.com/dydx/demo"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot demo.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
